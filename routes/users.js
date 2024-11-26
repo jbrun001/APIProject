2d49c08ed40e8f805d540814d57fcd8b186610e8
jbrun001 <jbrun001@gold.ac.uk>
2024-11-26 10:19:16 +0000

    update redirect links to be relative

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
-        res.redirect('/users/login') // redirect to the login page
+        res.redirect('users/login') // redirect to the login page
     } else { 
         next (); // move to the next middleware function
     } 
