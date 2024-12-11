878f05255e15ef8729296b9e6a427d0a9ecb7f53
jbrun001 <jbrun001@gold.ac.uk>
2024-12-11 16:43:40 +0000

    Implement validation and sanitisation for transactionsAdd

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const { validateAndSanitiseUsers } = require('../middleware/validateAndSanitiseInput');
+const { validateAndSanitiseTransactions } = require('../middleware/validateAndSanitiseInput');
 
 // get the start of the URL from index.js
 const { ORIGIN_URL } = require('../index.js');
@@ -25,23 +25,45 @@ router.get('/list', redirectLogin,function(req, res, next) {
 
 
 router.post('/add', redirectLogin, function (req, res, next) {
-    res.render("transactionsAdd.ejs",{fund_id:req.body.fund_id, portfolio_id:req.body.portfolio_id})
+    res.render('transactionsAdd.ejs', {
+        fund_id:req.body.fund_id, 
+        portfolio_id:req.body.portfolio_id,
+        fund_name: req.body.fund_name,
+        previousData: {}, // empty object for data previously entered
+        messages: [],     // array for validation messages
+    });
 })
 
-
-router.post('/added', redirectLogin,function (req, res, next) {
-    // saving data in database
-    let sqlquery = "INSERT INTO transactions (user_id, fund_id, portfolio_id, volume, share_price) VALUES (?,?,?,?,?)"
-    // execute sql query
-    let newrecord = [req.session.userId, req.body.fund_id, req.body.portfolio_id, req.body.volume, req.body.share_price]
-    db.query(sqlquery, newrecord, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        else
-            res.render("/funds/list",{portfolio_id:req.body.portfolio_id});
-    })
+router.post('/added', validateAndSanitiseTransactions, redirectLogin, function (req, res, next) {
+    // check if validation errors and if yes then re-display page with old data and error messages
+    if (req.validationErrors) {
+        // debug to test data is there
+        console.log({ success: false, previousData: req.body, messages: req.validationErrors });
+        // if there are errors then send the old data and the messages to the form
+        // so they can be displayed
+        return res.render('transactionsAdd.ejs', {
+            fund_id: req.body.fund_id, 
+            portfolio_id: req.body.portfolio_id,
+            fund_name: req.body.fund_name,
+            previousData: req.body,
+            messages: req.validationErrors,
+        });
+    }
+    else {
+        // saving data in database
+        let sqlquery = "INSERT INTO transactions (user_id, fund_id, portfolio_id, volume, share_price) VALUES (?,?,?,?,?)"
+        // execute sql query
+        let newrecord = [req.session.userId, req.body.fund_id, req.body.portfolio_id, req.body.volume, req.body.share_price]
+        db.query(sqlquery, newrecord, (err, result) => {
+            if (err) {
+                next(err)
+            }
+            else
+                res.render("fundsList.ejs",{portfolio_id:req.body.portfolio_id})
+        })
+    } 
 }) 
+
 
 
 router.get('/remove', redirectLogin, function (req, res, next) {
